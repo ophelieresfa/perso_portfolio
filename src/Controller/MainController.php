ae8b19403763ae3de1c20b0f33cc5597c80fc3ac
Ophélie <resfa.ophelie@yahoo.fr>
2020-04-28 20:11:41 +0200

    Using Extension to the MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Controller\Extension\Extension;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
@@ -12,6 +13,8 @@ class MainController
     public function __construct()
     {
         $this->twig = new Environment(new FilesystemLoader('../src/View'), array('cache' => false));
+        $this->twig->addExtension(new Extension());
+
     }
 
     public function redirect(string $page, array $params = [])
@@ -20,5 +23,12 @@ class MainController
         header('Location: index.php?' . http_build_query($params));
 
         exit;
+    }
+
+    public function url(string $page, array $params = [])
+    {
+        $params['action'] = $page;
+
+        return 'index.php?' . http_build_query($params);
     }
 }
