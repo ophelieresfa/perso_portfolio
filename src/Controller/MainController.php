2570f40f5d32e0050887e87622000024a95de925
Ophélie <resfa.ophelie@yahoo.fr>
2020-06-10 01:20:25 +0200

    Update the file MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,7 @@ use App\Controller\Extension\Extension;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-class MainController
+abstract class MainController extends GlobalController
 {
     protected $twig = null;
 
@@ -14,7 +14,6 @@ class MainController
     {
         $this->twig = new Environment(new FilesystemLoader('../src/View'), array('cache' => false));
         $this->twig->addExtension(new Extension());
-
     }
 
     public function redirect(string $page, array $params = [])
