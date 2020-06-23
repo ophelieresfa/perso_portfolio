4a0fc6de8273eccef081e876c01a954b9ec9eb15
Ophélie <resfa.ophelie@yahoo.fr>
2020-06-23 13:23:36 +0200

    Update the file QuoteController.php

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -17,14 +17,18 @@ class QuoteController extends MainController
         $firstname = $_POST["firstname"];
         $email = $_POST["email"];
         $about = $_POST["about"];
+        $quality = $_POST["vector"];
+        $budget = $_POST['budget'];
 
-        echo $name . " " . $firstname . " " . $email . " " . $about;
+        echo $name . " " . $firstname . " " . $email . " " . $about . " " . $quality . " " . $budget;
 
         if (!empty($_POST)) {
             $data['name'] = $_POST['name'];
             $data['firstname'] = $_POST['firstname'];
             $data['email'] = $_POST['email'];
             $data['about'] = $_POST['about'];
+            $data['vector'] = $_POST['vector'];
+            $data['budget'] = $_POST['budget'];
 
             ModelFactory::getModel('Quote')->createData($data);
 
